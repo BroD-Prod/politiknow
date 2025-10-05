c9f8a8ec81ab0ca24422f477a0bb2ae8b39a304c
Brody Roth <brodyroth23@gmail.com>
2025-10-05 04:02:34 -0400

    Recommitting project to re-align focuses. Backend devlopemnt was first step in this project, since it was the primary goal with rust.
    Essentialy restarting the project from the main starting point of json parsing and api requests returned to localhost server.

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SessionList {
+    status: String,
+    sessions: Vec<Session>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Session {
+    session_id: Option<u32>,
+    state_id: Option<u32>,
+    state_abbr: Option<String>,
+    year_start: Option<u32>,
+    year_end: Option<u32>,
+    prefile: Option<u32>,
+    sins_die: Option<u32>,
+    special: Option<u32>,
+    session_tag: Option<String>,
+    session_title: Option<String>,
+    session_name: Option<String>,
+    dataset_hash: Option<String>,
+}
+
+fn load_env() -> String {
+    dotenv::dotenv().ok();
+
+    let api_key = dotenv::var("API_KEY").expect("API_KEY must be set");
+    return api_key;
+}
+
+fn parse_session_list(json_data: &str) -> Result<SessionList, serde_json::Error> {
+   let resp: SessionList = serde_json::from_str(json_data)?;
+    Ok(resp)
+}
+
+fn parse_session_date(json_data: &str) -> Result<Vec<String>, serde_json::Error> {
+   let list = parse_session_list(json_data)?;
+   let names = list.sessions.iter().map(|s| s.session_name.clone().unwrap_or_default()).collect::<Vec<String>>();
+   Ok(names)
+}
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello, World!")
+}
+
+#[get("/sessions")]
+async fn get_session_list() -> actix_web::Result<HttpResponse> {
+    // Fetch remote body. Map reqwest errors into Actix internal errors so Actix can return 500s.
+    let api_key = load_env();
+    let resp_text = reqwest::get(format!("https://api.legiscan.com/?key={}&op=getSessionList&state=IN", api_key)).await
+        .map_err(actix_web::error::ErrorInternalServerError)?
+        .text().await
+        .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    parse_session_list(&resp_text).map_err(actix_web::error::ErrorInternalServerError)?;
+
+    // Return the fetched body to the browser. Set a content type if you know the format.
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain; charset=utf-8")
+        .body(resp_text))
+}
+
+#[get("/sessions_name")]
+async fn get_session_names() -> actix_web::Result<HttpResponse> {
+    // Fetch remote body. Map reqwest errors into Actix internal errors so Actix can return 500s.
+    let api_key = load_env();
+    let resp_text = reqwest::get(format!("https://api.legiscan.com/?key={}&op=getSessionList&state=IN", api_key)).await
+        .map_err(actix_web::error::ErrorInternalServerError)?
+        .text().await
+        .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    let names = parse_session_date(&resp_text).map_err(actix_web::error::ErrorInternalServerError)?;
+
+    // Return the fetched body to the browser. Set a content type if you know the format.
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain; charset=utf-8")
+        .body(names.join(", ")))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+          .service(greet)
+          .service(get_session_list)
+          .service(get_session_names)
+    })
+   .bind("127.0.0.1:8080")?
+   .run()
+   .await
+}
